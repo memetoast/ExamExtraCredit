cd5535b016f92b7c755997f75265a31a1a8a8e4e
memetoast <72702287+memetoast@users.noreply.github.com>
2022-09-26 12:38:50 -0500

    Add project files.

---
+++ ExamExtraCredit/Program.cs
@@ -0,0 +1,27 @@
+﻿//Andrew Moore
+//Extra Credit
+
+Console.WriteLine("Extra 1) In how many ways you can pass parameters to a method in C# (2 Points)" +
+    "\na.Value parameters" +
+    "\nb.Reference parameters" +
+    "\nc.Output parameters" +
+    "\nd.A & C" +
+    "\ne.All the above");
+Console.WriteLine("Answer: e. All the above\n");
+
+Console.WriteLine("Extra 2) To make it easy to test a class library, you can: (2 Points)" +
+    "\na.Make sure that all of the classes require only the System namespace" +
+    "\nb.Put references to all of the.NET namespaces in the library" +
+    "\nc.Add one class to the library at a time" +
+    "\nd.Develop the class library project within a solution that contains a Windows Forms project");
+Console.WriteLine("Answer: d. Develop the class library project within a solution that contains a windows Forms project\n");
+
+Console.WriteLine("\nExtra 3) What is the difference between ref and out keywords? (4 Points)");
+Console.WriteLine("Answer: The ref keyword and out keywords are both used to pass parameters by reference. The difference being" +
+    "\nthe variable marked with the out keyword has to be initialized inside of the method.\n");
+
+Console.WriteLine("\nExtra 4) What is the difference between Interface and Abstract Class? (4 Points)");
+Console.WriteLine("Answer: An abstract class is a group of properties things might share. Like a car, boat, and motorcycle all \n" +
+    "being a vehicle.\n" +
+    "An interface is a common method that can be implemented in different ways. Like the speed and maneuverability of\n" +
+    "each car, boat, and motorcycle.");
